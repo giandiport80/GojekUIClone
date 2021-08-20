1e508492943740791da08cdf4cb7c58d9d73bc1e
giannurwana19 <giannurwana19@gmail.com>
2021-08-20 22:45:24 +0700

    Go-News section part 2

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   Text,
   TextInput,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
@@ -387,21 +388,87 @@ const App = () => {
             backgroundColor: '#f2f2f4',
             marginTop: 20,
           }}></View>
+
         {/* news section */}
-        <View style={{paddingTop: 16, paddingHorizontal: 17}}>
-          <View>
+        <View
+          style={{
+            paddingTop: 16,
+            paddingHorizontal: 16,
+          }}>
+          <View style={{position: 'relative'}}>
             <Image
               source={require('./src/assets/dummy/sepak-bola.jpg')}
               style={{height: 170, width: '100%', borderRadius: 6}}
             />
+            <View
+              style={{
+                width: '100%',
+                height: '100%',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                backgroundColor: '#000',
+                opacity: 0.2,
+                borderRadius: 6,
+              }}></View>
+
+            <View
+              style={{
+                height: 15,
+                width: 55,
+                position: 'absolute',
+                top: 16,
+                left: 16,
+              }}>
+              <Image
+                source={require('./src/assets/logo/white.png')}
+                style={{
+                  width: undefined,
+                  height: undefined,
+                  flex: 1,
+                  resizeMode: 'contain',
+                }}
+              />
+            </View>
           </View>
-          <View style={{paddingTop: 16, paddingBottom: 20}}>
+          <View
+            style={{
+              paddingTop: 16,
+              paddingBottom: 20,
+              borderBottomColor: '#e8e9ed',
+              borderBottomWidth: 1,
+              marginBottom: 20,
+            }}>
             <Text style={{fontSize: 16, fontWeight: 'bold', color: '#1c1c1c'}}>
               GO-NEWS
             </Text>
-            <Text style={{fontSize: 14, fontWeight: 'normal', color: '#7a7a7a'}}>
+            <Text
+              style={{
+                fontSize: 14,
+                fontWeight: 'normal',
+                color: '#7a7a7a',
+                marginBottom: 11,
+              }}>
               Dimas Drajat selamatkan penali, timnas U-23 kalahkan Brunei
             </Text>
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#61a756',
+                paddingHorizontal: 12,
+                paddingVertical: 11,
+                alignSelf: 'flex-end',
+                borderRadius: 4,
+                textAlign: 'center',
+              }}>
+              <Text
+                style={{
+                  fontSize: 13,
+                  fontWeight: 'bold',
+                  color: '#fff',
+                }}>
+                READ
+              </Text>
+            </TouchableOpacity>
           </View>
         </View>
       </ScrollView>
