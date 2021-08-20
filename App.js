f34f9d4fac37fbea7537840cdb1ff399ba99a1e2
giannurwana19 <giannurwana19@gmail.com>
2021-08-20 19:33:24 +0700

    search bar dan promo section

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, TextInput, View} from 'react-native';
 
 const App = () => {
   return (
     <View style={{flex: 1}}>
       <View style={{flex: 1, backgroundColor: 'pink'}}>
-        <Text>Clone Gojek App</Text>
+        <View
+          style={{marginHorizontal: 17, flexDirection: 'row', paddingTop: 15}}>
+          <View style={{position: 'relative', flex: 1}}>
+            <TextInput
+              placeholder="What do you want to eat?"
+              style={{
+                backgroundColor: '#fff',
+                borderWidth: 1,
+                borderColor: '#e8e8e8',
+                borderRadius: 25,
+                height: 40,
+                fontSize: 13,
+                paddingLeft: 45,
+                paddingRight: 20,
+                marginRight: 18,
+              }}
+            />
+            <Image
+              source={require('./src/assets/icon/search.png')}
+              style={{position: 'absolute', top: 5, left: 12}}
+            />
+          </View>
+          <View
+            style={{
+              width: 35,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Image source={require('./src/assets/icon/promo.png')} />
+          </View>
+        </View>
       </View>
       <View style={{height: 54, flexDirection: 'row', backgroundColor: '#fff'}}>
         <View
